ccf23d9dd072480fe73a6c82102fc2e7a8c06bb5
hangyeol0531 <41174265+hangyeol0531@users.noreply.github.com>
2020-02-04 00:27:26 +0900

    complete

---
+++ Discrod.js project/LOL.js
@@ -0,0 +1,9 @@
+const School = require('node-school-kr');
+const Discord = require('discord.js');
+const config = require('../config.json');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  });
+client.login(config.token)
