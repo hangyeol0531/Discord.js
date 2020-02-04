d87762cf37bd3f03740c58c3add690857c405e5e
hangyeol0531 <41174265+hangyeol0531@users.noreply.github.com>
2020-02-05 01:11:26 +0900

    make

--- Discrod.js project/LOL.js
+++ Discrod.js project/LOL.js
@@ -6,4 +6,5 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
   });
+  
 client.login(config.token)
--- Discrod.js project/schoolfoodbot.js
+++ Discrod.js project/schoolfoodbot.js
@@ -8,6 +8,9 @@ const today = new Date();
 const todayyear = today.getFullYear();
 const todaymonth = today.getMonth()+1;
 const todayday = today.getDate();
+let lee_secret = 0;
+let attack_user = "지우";
+let stauts = "명령어가 궁금 하시면 \"하이의현 명령어\" 를 입력해주세요!"
 school.init(School.Type.HIGH, School.Region.GWANGJU, 'F100000120');//120
 
 const mealAsync = async function(msg) {
@@ -36,22 +39,49 @@ const calAsync = async function(msg) {
     }
 }
 
-client.on('message', msg =>{ //급식 
+client.on('message', msg =>{ //급식  일정
     if(msg.content == 'gsm급식' || msg.content == 'GSM급식'){
-        console.log(school.getTargetURL('meal', 2018, 5))
+        //console.log(school.getTargetURL('meal', 2018, 5))
         mealAsync(msg);
-    } 
-});
-
-client.on('message', msg =>{ //일정
-    if(msg.content == 'gsm일정' || msg.content == 'GSM일정'){
+    }else if(msg.content == 'gsm일정' || msg.content == 'GSM일정'){ //일정
         //console.log(school.getTargetURL('calendar')) 
         calAsync(msg);
     }
 });
 
+client.on('message', msg =>{
+    console.log(`author.username = ${msg.author.username}`)
+    if((msg.author.username == attack_user) && (lee_secret == 1)){
+        msg.channel.send(`${attack_user}아 넌 좀 조용히해`); 
+    }else if(msg.content == '하이의현'){
+        console.log(msg.author.username);
+        msg.reply(`안냥!!`);
+    }else if(msg.content == '하이의현 명령어'){
+        msg.channel.send("```" + `하이의현 : 의현이가 상큼하게 인사합니다.\n\ngsm일정 : 오늘 하루의 gsm일정을 알려줍니다. \n\ngsm급식 : 오늘의 하루의 gsm급식을 알려줍니다.` + "```");
+    }
+
+    if(msg.content == '하이의현 공격' && (msg.author.username != attack_user)){
+        if(lee_secret == 1){
+            msg.channel.send(`이미 ${attack_user} 를 공격중입니다.`); 
+        }else{
+            lee_secret = 1;
+            msg.channel.send(`이의현 공격 모드 시작  대상 : ${attack_user}`);     
+        }
+    }else if(msg.content == '하이의현 공격 종료' && (msg.author.username != attack_user)){
+        if(lee_secret == 0){
+            msg.channel.send("이미 종료 되었습니다."); 
+        }else{
+            lee_secret = 0;
+            msg.channel.send("이의현 공격 모드 종료");     
+        }
+    }
+});
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    setInterval(function() {
+    client.user.setPresence({game : {name : stauts}, status : 'online'});
+    }, 10000)
   });
 client.login(config.token)
 
