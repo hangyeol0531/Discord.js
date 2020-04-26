5b841217ea3d83dca92b3ee4882327dd0be262d0
hangyeol0531 <jhan0531@naver.com>
2020-04-27 00:38:58 +0900

    um_check_bot

---
+++ Discrod.js project/um_check/bot.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = 'NjcyOTM5NDU4ODE1MDY2MTEy.XqWAOA.lpDZy1bSx01cd_k3deqLF6ndUSg';
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    let check_cotent;
+    let check_array = [ ];
+  if (msg.content.slice(0,3) === '검준식') {
+    console.log("검준식 가동");
+    check_cotent = msg.content.slice(3, msg.content.length+1);
+    console.log(check_cotent);
+    
+    if(check_cotent.includes('UM')){
+        check_array.push('UM');
+    }
+    if(check_cotent.includes('JUN')){
+        check_array.push('JUN');
+    }
+    if(check_cotent.includes('SIK')){
+        check_array.push('SIK');
+    }
+    if(check_cotent.includes('엄')){
+        check_array.push('엄');
+    }
+    if(check_cotent.includes('준')){
+        check_array.push('준');
+    }
+    if(check_cotent.includes('식')){
+        check_array.push('식');
+    }
+    console.log(check_array);
+    if(check_array == ''){
+        msg.reply("깨끗한 엄입니다.");
+    }else{
+        msg.reply(`${check_array}은 살아있다.`);
+    }
+  }
+});
+
+client.login(token)
