a4ce233019e4d97f6d44a310f58d1340498640fe
hangyeol0531 <jhan0531@naver.com>
2020-07-05 05:05:52 +0900

    update

--- Discrod.js project/Nan Gaek/bot.js
+++ Discrod.js project/Nan Gaek/bot.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('../../config.json');
 const mysql = require('mysql');
+const { isUndefined } = require('util');
 
 var db = mysql.createConnection({
   host: "localhost",
@@ -10,47 +11,69 @@ var db = mysql.createConnection({
   password: config.sqlpassword
 })
 
-let player_count;
-let global_rows;
 
-const select_information = async () =>{
-  var sql = `SELECT * FROM user_information`
-  await db.query(sql, async function(err, rows){
-    if(!err) {
-      global_rows = rows;
-      player_count = rows.length;
-      console.log("select_inforation");
-    }else{
-        console.log(err);
-    }   
-  })
-}
-
-client.on('message', async msg => { 
-
-  if(msg.content.includes('!참가')){
-    await select_information();
-    player_count += 1;
-    sql = `insert into user_information(name) VALUES("${msg.author.username}")`;
-    await db.query(sql, function(err, rows){
+client.on('message', async msg => {
+  if(msg.channel.name.includes('낸객')){
+    if(msg.content.includes('!참가')){
+      var player_count;
+      var sql = `SELECT * FROM user_information`
+      await db.query(sql, function(err, rows){
         if(!err) {
-            console.log("!참가 입력 성공");
-            msg.reply(`"${msg.author.username}"으로 정상적으로 등록되었습니다. (8/${player_count})`)
+          player_count = rows.length + 1;
+          console.log("검색 성공");
         }else{
             console.log(err);
         }   
-    })
-  }
-
-  if(msg.content.includes('!리스트')){
-      await select_information();
-      console.log(global_rows)
-      console.log(player_count)
-      let player_list = ''
-      for (var i in player_count){
-        player_list  += global_rows[i].name + '/n';
-      }
-      msg.reply(`''' ${player_list} '''`)
+      })
+      sql = `SELECT * FROM user_information where discord_id = "${msg.author.id}"`
+        await db.query(sql, async function(err, rows){
+          if(!err) {
+            if(!rows[0]){
+              sql = `insert into user_information(name, discord_id) VALUES("${msg.author.username}", "${msg.author.id}")`;
+              await db.query(sql, async function(err, rows){
+                  if(!err) {
+                      console.log("입력 성공");
+                      msg.reply(`"${msg.author.username}"으로 정상적으로 등록되었습니다. (8/${player_count})`)
+                  }else{
+                      console.log(err);
+                  }   
+              })
+            }else{
+              msg.reply('이미 등록되어 있는 선수입니다.')
+            }
+          }else{
+              console.log(err);
+          }   
+      })
+    }
+  
+    if(msg.content.includes('!리스트')){
+     
+      var sql = `SELECT * FROM user_information`
+      var player_list = ''
+      db.query(sql, function(err, rows){
+        if(!err) {
+          for(var i in rows){
+            player_list  += Number(i) +1 + '.' +rows[i].name + '\n';
+            console.log(rows[i].name)
+          }
+          msg.reply("```" + player_list + "```" )
+        }else{
+            console.log(err);
+        }   
+      })
+    }
+  
+    if(msg.content.includes('!초기화')){
+      var sql = `DELETE FROM user_information`
+      db.query(sql, function(err, rows){
+        if(!err) {
+          msg.reply("초기화되었습니다.")
+        }else{
+            console.log(err);
+        }   
+      })
+    }
   }
 })
 
