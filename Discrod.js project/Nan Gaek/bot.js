2f2bc204c86278491ad46380dd220be0c3a5ce60
hangyeol0531 <jhan0531@naver.com>
2020-07-05 05:39:42 +0900

    complete

--- Discrod.js project/Nan Gaek/bot.js
+++ Discrod.js project/Nan Gaek/bot.js
@@ -13,37 +13,41 @@ var db = mysql.createConnection({
 
 
 client.on('message', async msg => {
-  if(msg.channel.name.includes('낸객')){
+  if(msg.channel.name.includes('bot') ){
     if(msg.content.includes('!참가')){
       var player_count;
       var sql = `SELECT * FROM user_information`
-      await db.query(sql, function(err, rows){
+      await db.query(sql, async function(err, rows){
         if(!err) {
           player_count = rows.length + 1;
           console.log("검색 성공");
+          if(player_count <= 8){
+            sql = `SELECT * FROM user_information where discord_id = "${msg.author.id}"`
+            await db.query(sql, async function(err, rows){
+              if(!err) {
+                if(!rows[0]){
+                  sql = `insert into user_information(name, discord_id) VALUES("${msg.author.username}", "${msg.author.id}")`;
+                  await db.query(sql, async function(err, rows){
+                      if(!err) {
+                          console.log("입력 성공");
+                          msg.reply(`"${msg.author.username}"으로 정상적으로 등록되었습니다. (8/${player_count})`)
+                      }else{
+                          console.log(err);
+                      }   
+                  })
+                }else{
+                  msg.reply('이미 등록되어 있는 선수입니다.')
+                }
+              }else{
+                  console.log(err);
+              }   
+          })
+        }else{
+          msg.reply(`사람이 가득 찼습니다!! (8/${player_count - 1})`)
+        }
         }else{
             console.log(err);
         }   
-      })
-      sql = `SELECT * FROM user_information where discord_id = "${msg.author.id}"`
-        await db.query(sql, async function(err, rows){
-          if(!err) {
-            if(!rows[0]){
-              sql = `insert into user_information(name, discord_id) VALUES("${msg.author.username}", "${msg.author.id}")`;
-              await db.query(sql, async function(err, rows){
-                  if(!err) {
-                      console.log("입력 성공");
-                      msg.reply(`"${msg.author.username}"으로 정상적으로 등록되었습니다. (8/${player_count})`)
-                  }else{
-                      console.log(err);
-                  }   
-              })
-            }else{
-              msg.reply('이미 등록되어 있는 선수입니다.')
-            }
-          }else{
-              console.log(err);
-          }   
       })
     }
   
@@ -69,6 +73,36 @@ client.on('message', async msg => {
       db.query(sql, function(err, rows){
         if(!err) {
           msg.reply("초기화되었습니다.")
+        }else{
+            console.log(err);
+        }   
+      })
+    }
+    
+    if(msg.content.includes('!팀')){
+      var player_count
+      var sql = `SELECT * FROM user_information`
+      var player_list = ''
+      await db.query(sql, function(err, rows){
+        if(!err) {
+          player_count = rows.length
+          console.log(`선수 검색 완료 : ${player_count}`)
+          if(player_count == 8){
+            let players = new Array(8);
+            let team1 = new Array(4);
+            let team2 = new Array(4);
+            for(var i in rows){
+              players[i] = rows[i].name;
+            }
+            var players_shuffle = players.sort(function(){return 0.5-Math.random()});
+            console.log(players_shuffle)
+            team1 = players[0] + " , " + players[1] + " , " + players[2] + " , " + players[3];
+            team2 = players[4] + " , " + players[5] + " , " + players[6] + " , " + players[7];
+            msg.channel.send("```" + '팀1: '  + team1 + "```" + '\n' + "```" + '팀2: '  + team2 + "```")
+            
+          }else{
+            msg.channel.send('선수가 부족합니다.')
+          }
         }else{
             console.log(err);
         }   
